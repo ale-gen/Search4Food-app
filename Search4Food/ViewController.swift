bfed38ecd992ffa3414b0dd38e828f29fafb63d4
Aleksandra Generowicz <a.generowicz@wp.pl>
2021-06-29 10:31:49 +0200

    Initial Commit

---
+++ Search4Food/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Search4Food
+//
+//  Created by Aleksandra Generowicz on 29/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
