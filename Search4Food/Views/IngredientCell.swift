c418aa515d349ef83614e3f38297c38a6634c375
Aleksandra Generowicz <a.generowicz@wp.pl>
2021-06-29 22:53:31 +0200

    Fix ingredient cells font size

--- Search4Food/Views/IngredientCell.swift
+++ Search4Food/Views/IngredientCell.swift
@@ -14,6 +14,7 @@ class IngredientCell: UITableViewCell {
     @IBOutlet weak var ingredientAmountLabel: UILabel!
     
     
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         ingredientBubble.layer.cornerRadius = ingredientBubble.frame.height / 5
